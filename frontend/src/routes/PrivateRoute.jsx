5163e08abaea851a493a1cfcf58ed0d2b3a32907
Goutam <goutamrajpal136@gmail.com>
2025-06-08 20:40:39 +0500

    Implement Private Route

--- frontend/src/routes/PrivateRoute.jsx
+++ frontend/src/routes/PrivateRoute.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { useContext } from "react";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+import Loading from "../components/Loading";
 
-function PrivateRoute() {
-  return (
-    <Outlet />
-  )
-}
+const PrivateRoute = ({ allowedRoles }) => {
+  const { user,loading } = useContext(UserContext);
+  const location = useLocation();
 
-export default PrivateRoute
+  if (loading) {
+    return <Loading />;
+  }
+
+
+  
+  if (!user) {
+    return <Navigate to="/login" replace state={{ from: location }} />;
+  }
+  
+
+  if (!allowedRoles.includes(user?.role)) {
+    console.log("Unauthorized");
+    
+    if (user.role === "admin") {
+      return <Navigate to="/admin/dashboard" replace state={{ from: location }}/>;
+    } else if (user.role === "member") {
+      return <Navigate to="/user/dashboard" replace state={{ from: location }}/>;
+    }
+    return <Navigate to="/login" replace state={{ from: location }}/>;
+
+  }
+
+  return <Outlet />;
+};
+
+export default PrivateRoute;
